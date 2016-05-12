c5ab39e95d1503c4639e0ef02ef4a8d0d11c2d04
Raphael <raphaelschaer81@gmail.com>
2016-05-12 21:44:43 +0200

    adding config file

--- config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-	"ga_code": "UA-77518854-1" //Google Analytics Trakcing ID: UA-xxxxxxx-x
-}
-
-module.exports = config;
---
+++ config.sample.js
@@ -0,0 +1,5 @@
+var config = {
+	"ga_code": "UA-xxxxxxx-x-1" //Google Analytics Trakcing ID: UA-xxxxxxx-x
+}
+
+module.exports = config;
